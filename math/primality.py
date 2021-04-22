e9504b636ee181a0cbd2a7e0dfd9bbb042993757
nabepa <nabepadev@gmail.com>
2021-04-22 23:11:49 +0900

    primality.py: add function which returns prime numbers until n

--- math/primality.py
+++ math/primality.py
@@ -17,6 +17,29 @@ def check_primality(n):
     return True if cnt <= 2 else False
 
 
+# n까지의 소수 찾기
+# Find prime numbers until n
+def find_prime_numbers(n):
+    array = [True for i in range(n + 1)]
+
+    # Eratosthenes' sieve
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if array[i] == True:  # i가 남은 수인 경우
+            # i를 제외한 i의 모든 배수 지우기
+            j = 2
+            while i * j <= n:
+                array[i * j] = False
+                j += 1
+
+    result = []
+    for i in range(2, n + 1):
+        if array[i]:
+            result.append(i)
+
+    return result
+
+
 if __name__ == '__main__':
     n = int(input())
     print(check_primality(n))
+    print(find_prime_numbers(n))
