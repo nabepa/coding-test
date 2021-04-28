f0fdd92ce449e19802be7f3ad0a1e5a82d54f34b
nabepa <nabepadev@gmail.com>
2021-04-28 22:17:18 +0900

    add find nth prime number algorithm

--- Math/primality.py
+++ Math/primality.py
@@ -1,6 +1,6 @@
 # 소수 판별
 # Primality test
-import math
+from math import sqrt, log, ceil
 
 
 # 주어진 수가 소수인지 판별
@@ -8,7 +8,7 @@ import math
 def check_primality(n):
     cnt = 0
     i = 1
-    while i <= math.sqrt(n):
+    while i <= sqrt(n):
         if n % i == 0:
             cnt += 2
             if cnt > 2:
@@ -19,11 +19,11 @@ def check_primality(n):
 
 # n까지의 소수 찾기
 # Find prime numbers until n
-def find_prime_numbers(n):
+def find_primes_until(n):
     array = [True for i in range(n + 1)]
 
     # Eratosthenes' sieve
-    for i in range(2, int(math.sqrt(n)) + 1):
+    for i in range(2, int(sqrt(n)) + 1):
         if array[i] == True:  # i가 남은 수인 경우
             # i를 제외한 i의 모든 배수 지우기
             j = 2
@@ -39,7 +39,22 @@ def find_prime_numbers(n):
     return result
 
 
+# n번째 소수
+# n번째 소수가 있을 하한, 상한이 있음(prime number theorem)
+# 상한값 까지의 소수를 Eratosthenes' sieve로 구하고, n번째 추출
+# https://blog.naver.com/kyh941031/221634481228 참고
+def find_nth_prime(n):
+    if n < 6:
+        primes = [2, 3, 5, 7, 11]
+        return primes[n - 1]
+    else:
+        limit = ceil(n * log(n) + n * log(log(n)))
+        primes = find_primes_until(limit)
+        return primes[n - 1]
+
+
 if __name__ == '__main__':
     n = int(input())
     print(check_primality(n))
-    print(find_prime_numbers(n))
+    print(find_primes_until(n))
+    print(find_nth_prime(n))
