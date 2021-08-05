fd6ac7116200a49a021fc51deb26dce80483ad5d
nabepa <nabepadev@gmail.com>
2021-08-05 21:35:25 +0900

    fail

--- baekjoon/math/3343.py
+++ baekjoon/math/3343.py
@@ -8,7 +8,6 @@ if bunch1 < bunch2:
 num1 = n // bunch1
 if n % bunch1 != 0:
     num1 += 1
-# num2 = 0
 
 prev_cost = int(1e9)
 cur_cost = cost1 * num1
@@ -17,9 +16,9 @@ while prev_cost >= cur_cost:
     num1 -= 1
     remain = n - num1 * bunch1
     num2 = remain // bunch2
-    if remain // bunch2 != 0:
+    if remain % bunch2 != 0:
         num2 += 1
-    cur_cost = num1 * bunch1 + num2 * bunch2
+    cur_cost = num1 * cost1 + num2 * cost2
 
 print(prev_cost)
 
